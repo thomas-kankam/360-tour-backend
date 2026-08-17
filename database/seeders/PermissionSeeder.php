6788bd1484f7333772f2c6d27e86253b37af7dbb
thomas-kankam <thomas.kankam@gwosevo.com>
2026-08-17 12:45:11 +0000

    Add new permissions for rating, invoice, and CMS management in PermissionSeeder

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -11,12 +11,15 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $permissions = [
-            ['name' => 'user_management', 'label' => 'User Management'],
+            ['name' => 'user_management',    'label' => 'User Management'],
             ['name' => 'listing_management', 'label' => 'Listing Management'],
             ['name' => 'booking_management', 'label' => 'Booking Management'],
-            ['name' => 'client_management', 'label' => 'Client Management'],
+            ['name' => 'client_management',   'label' => 'Client Management'],
             ['name' => 'contact_management', 'label' => 'Contact Management'],
-            ['name' => 'role_management', 'label' => 'Role Management'],
+            ['name' => 'role_management',     'label' => 'Role Management'],
+            ['name' => 'rating_management',   'label' => 'Rating Management'],
+            ['name' => 'invoice_management',  'label' => 'Invoice Management'],
+            ['name' => 'cms_management',      'label' => 'CMS Management'],
         ];
 
         foreach ($permissions as $permission) {
